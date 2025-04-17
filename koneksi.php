295b1327fc687879c1a058be6d65497c537398ca
phpcode.id <41561993+Myhermanto@users.noreply.github.com>
2025-04-17 13:28:14 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,15 @@
 
+<?php
+$servername = "localhost"; // Alamat server database
+$username = "root";        // Username database
+$password = "";            // Password database
+$dbname = "pet_shop";      // Nama database
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
+}
+?>
